04659b938cbda0d670ac699d0cbbaf09cb9fd986
Davide Principi <davide.principi@nethesis.it>
2024-09-18 17:23:24 +0200

    Implement PECBRIDGE_ADMIN_MAIL env var
    If PECBRIDGE_ADMIN_MAIL is added to the environment file, Pec Bridge
    uses its value to expand the XML configuration file. The value must be
    an email address, which Pec Bridge uses to send a copy of notification
    messages.

--- webapp/usr/share/pecbridge/bin/run.sh
+++ webapp/usr/share/pecbridge/bin/run.sh
@@ -5,15 +5,19 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 #
 
+set -e
+
 PBHOME="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )"
 
 cd "$PBHOME"
 
-CLASSPATH=lib/*:classes
+CLASSPATH="lib/*:classes"
 export PATH CLASSPATH
 
 # Expand the Pecbridge configuration file template:
-sed "s/WEBAPP_API_TOKEN/${WEBAPP_API_TOKEN:?}/" etc/config.xml.template > etc/config.xml
+sed -e "s/WEBAPP_API_TOKEN/${WEBAPP_API_TOKEN:?}/" \
+    ${PECBRIDGE_ADMIN_MAIL:+-e "1 s/sonicle-pec-bridge /sonicle-pec-bridge adminMail=\"${PECBRIDGE_ADMIN_MAIL}\" /"} \
+    etc/config.xml.template > etc/config.xml
 
 # Start the Pecbridge process
 exec java com.sonicle.pecbridge.Main etc/config.xml
